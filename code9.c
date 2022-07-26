643a47dbfc60182b094b1111f2f9150704fcc336
mrash17 <77162392+mrash17@users.noreply.github.com>
2022-07-26 18:47:26 +0530

    Update code9.c

--- code9.c
+++ code9.c
@@ -1,9 +1,10 @@
-//LCM
+//LCM 
+//Method 1
 #include <stdio.h>
 void main()
 {
   int n1,n2;
-  scnaf("%d %d",&n1,&n2);
+  scanf("%d %d",&n1,&n2);
   int gcd,lcm;
   for(int i=1;i<=n1 && i<=n2;i++)
   {
@@ -13,4 +14,23 @@ void main()
     }
   }
   lcm=n1*n2/gcd;
+}
+
+//Method 2
+
+#include <stdio.h>
+void main()
+{
+  int n1,n2;
+  scnaf("%d %d",&n1,&n2);
+  int max=(n1>n2)?n1:n2;
+  while(1)
+  {
+    if((max%n1==0) &&(max%n2==0))
+    { printf("The LCM is %d",max);
+      break;
+    }
+    max++;
+  }
+  
 }
