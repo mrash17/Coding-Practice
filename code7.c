e6a26f5a91786b3fa0a5e7afd38aebe2f1ddcace
mrash17 <77162392+mrash17@users.noreply.github.com>
2022-08-09 17:02:42 +0530

    Create code7.c

---
+++ code7.c
@@ -0,0 +1,29 @@
+import java.util.*;
+class arrange{
+  static void arrangeit(int arr[],int n)
+  {
+    for(int i=0;i<n;i++)
+    {
+      for(int j=i+1;j<n;j++)
+        {
+          int temp;
+          if(arr[i]>arr[j])
+          {
+            temp=arr[i];
+            arr[i]=arr[j];
+            arr[j]=temp;
+          }
+        }
+    }
+  }
+  public static void main (String args[])
+  {
+    int arr[]={-1,-3,2,5,3,-5};
+    int n=arr.length();
+    arrangeit(arr,n);
+    for(int i=0;i<n;i++)
+    {
+      System.out.print(arr[i]+" ");
+    }
+  }
+}
