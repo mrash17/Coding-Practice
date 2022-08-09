0f02d9d641d35e27b06c10a7ac4f845814e47072
mrash17 <77162392+mrash17@users.noreply.github.com>
2022-08-09 17:08:11 +0530

    Update code10.java

--- code10.java
+++ code10.java
@@ -27,3 +27,22 @@ class arrange{
     }
   }
 }
+
+#Using inbuilt Function
+import java.util.*;
+class arrange{
+  static void arrangeit(int arr[],int n)
+  {
+	  Arrays.sort(arr);
+  }
+  public static void main (String args[])
+  {
+    int arr[]={-1,-3,2,5,3,-5};
+    int n=arr.length;
+    arrangeit(arr,n);
+    for(int i=0;i<n;i++)
+    {
+      System.out.print(arr[i]+" ");
+    }
+  }
+}
