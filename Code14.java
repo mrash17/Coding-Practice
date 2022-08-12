400e95dbd3d11bf64c3c7aa6a6d6fd3188705042
Ashish Makhija <77162392+mrash17@users.noreply.github.com>
2022-08-12 14:39:02 +0530

    Update Code14.java

--- Code14.java
+++ Code14.java
@@ -1,19 +1,55 @@
-    class Solution {
-        public int lengthOfLongestSubstring(String s) {
-            if(s.length()==1){
-                return 1;
-            }
-            int count=0;
-            HashMap<Character,Integer> map=new HashMap<>();
-            int j=0;
-            for(int i=0;i<s.length();i++){
-                if(map.containsKey(s.charAt(i))){
-                    j=Math.max(j,map.get(s.charAt(i))+1);
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int n=s.length();
+        if(n==0||n==1)
+            return n;
+        int ans=0;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=i+1;j<=n;j++)
+            {
+               
+                if(allUniq(s,i,j))
+                {
+                   ans=Math.max(ans,j-i);
                 }
-                map.put(s.charAt(i),i);
-                count=Math.max(count,i-j+1);
-
             }
-            return count;
         }
+        
+        return ans;
     }
+    public boolean allUniq(String s,int start,int end)
+    {
+        Set<Character> set=new HashSet<>();
+        for(int k=start;k<end;k++)
+                {
+                    char ch=s.charAt(k);
+                    if(set.contains(ch)) return false;
+                    set.add(ch);
+        }
+        return true;          
+    }
+}
+
+
+
+
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        if(s.length()==1){
+            return 1;
+        }
+        int count=0;
+        HashMap<Character,Integer> map=new HashMap<>();
+        int j=0;
+        for(int i=0;i<s.length();i++){
+            if(map.containsKey(s.charAt(i))){
+                j=Math.max(j,map.get(s.charAt(i))+1);
+            }
+            map.put(s.charAt(i),i);
+            count=Math.max(count,i-j+1);
+            
+        }
+        return count;
+    }
+}
