e15399f7bf442355e54856f4b234d1add3fe1950
Ashish Makhija <77162392+mrash17@users.noreply.github.com>
2022-08-13 22:09:45 +0530

    Merging of two sorted arrays

---
+++ code16.java
@@ -0,0 +1,27 @@
+class Solution {
+    public static void merge(int arr1[],int arr2[],int arr3[],int n1,int n2)
+    {
+        int i=0;j=0,k=0;
+        while(i<n1 && j<n2)
+        {
+            if(arr1[i]<arr2[j])
+                arr3[k++]=arr1[i++];
+            else
+                arr3[k++]=arr2[j++];
+        }
+        while(i<n1)
+            arr3[k++]=arr1[i++];
+        while(j<n2)
+            arr3[k++]=arr2[j++];
+       
+    }
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int n1=nums.length;
+        int n2=nums.length;
+        int arr[]=new arr[n1+n2];
+        merge(nums1,nums2,arr,n1,n2);
+        
+        
+    }
+}
+//merging of two sorted arrays
